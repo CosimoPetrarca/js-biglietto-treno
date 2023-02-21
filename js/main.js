8eef1f621f675165b7da37fdcbaec1d4508f2a89
P4anix <pancok_1@yahoo.it>
2023-02-21 18:11:50 +0100

    fixed

--- js/main.js
+++ js/main.js
@@ -18,14 +18,12 @@ console.log("Prezzo totale senza sconti: " + prezzoTotale.toFixed(2) + " euro");
 /* applicare scontistica */
 if (eta < 18) {
     const sconto = prezzoTotale * 0.2;
-    prezzoTotale - sconto;
     const prezzoMinori = prezzoTotale - sconto;
+    /* prezzo finale */
     console.log("Sconto del 20% per i minorenni: " + sconto.toFixed(2) + " euro, quindi prezzo totale è " + prezzoMinori.toFixed(2) + " euro");
 } else if (eta >= 65) {
     const sconto = prezzoTotale * 0.4;
-    prezzoTotale - sconto;
     const prezzoOver = prezzoTotale - sconto;
+    /* prezzo finale */
     console.log("Sconto del 40% per gli over 65: " + sconto.toFixed(2) + " euro, quindi prezzo totale è " + prezzoOver.toFixed(2) + " euro");
 } else ('prezzo totale senza sconti');
-
-
