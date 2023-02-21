4ce48bb944cfa2611da860e98bfdc1dff8b55317
P4anix <pancok_1@yahoo.it>
2023-02-21 15:08:19 +0100

    first step

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/* chiedere la variabile chilometri con un prompt */
+const chilometri = prompt('Quanti chilometri dovrai percorrere?');
+console.log(chilometri);
+
+/* chiedere la variabile età del passeggero  con un prompt */
+const eta = prompt('Quanti anni ha il passeggero?');
+console.log(eta);
+
+/* inserire la variabile prezzo per chilometri */
+const prezzoPerKm = 0.21;
+
+/* calcolare la variabile prezzo in base ai chilometri */
+const prezzoTotale = chilometri * prezzoPerKm
+console.log(prezzoTotale);
+
+
+
+
+
